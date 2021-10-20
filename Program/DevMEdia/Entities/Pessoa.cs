a5b5eb5ba51da244dd4a8d971482a252a24e7cd8
Mauricio <mauriciovieira8@hotmail.com>
2021-10-20 14:43:27 -0300

    Introdução: CSharp Orientado a Objetos

---
+++ Program/DevMEdia/Entities/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿
+namespace DevMEdia.Entities
+{
+    public class Pessoa
+    {
+        public Pessoa(string cabelo)
+        {
+            Olhos = 2;
+            Bracos = 2;
+            Pernas = 2;
+            CorCabelos = cabelo;
+        }
+        public string Nome { get; set; }
+        public int Olhos { get; set; }
+        public string CorCabelos { get; set; }
+        public int Bracos { get; set; }
+        public int Pernas { get; set; }
+
+
+        public Pessoa()// construtor padrão. cria o objeto na memória
+        {
+
+        }
+
+
+
+
+        
+    }
+}
---
+++ Program/DevMEdia/Program.cs
@@ -0,0 +1,32 @@
+﻿using DevMEdia.Entities;
+using System;
+using System.Globalization;
+
+
+namespace DevMEdia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            p.Nome = ("Mauricio Luis Pantoja Vieira");
+            p.Bracos = 2;
+            p.Pernas = 2;
+            p.Olhos = 2;
+            p.CorCabelos = "Preto";
+            Console.WriteLine(p.Nome+" POssui "+
+                p.Bracos+" braços "+p.Pernas+" Pernas "+
+                p.Olhos+" olhos e cabelo "+p.CorCabelos+"\n");
+
+            Pessoa p1 = new Pessoa("Loiro");
+            p1.Nome = "Neymar Jr.";
+            Console.WriteLine(p1.Nome + ": POssui " +
+                p1.Bracos + " braços " + p1.Pernas + " Pernas " +
+                p1.Olhos + " olhos e cabelo " + p1.CorCabelos + "\n");
+
+
+            //  Console.WriteLine("Hello World!");
+        }
+    }
+}
